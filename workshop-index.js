e2045004e0b175039d4c1aa89c95b59d4934b7c9
Mark Conn <misterconn@gmail.com>
2016-10-24 19:15:14 +0000

    ex 3

--- workshop-index.js
+++ workshop-index.js
@@ -6,12 +6,42 @@ var app = express();
 //   response.send(`<h1>Hello ${name}!</h1>`);
 // });
 
-app.get('/hello/:name', function (request, response) {
-   var name = request.params.name
-   response.send(`<h1>Hello ${name}!</h1>`);
+// app.get('/hello/:name', function (request, response) {
+//   var name = request.params.name
+//   response.send(`<h1>Hello ${name}!</h1>`);
+// });
+
+app.get('/calculator/:operation', function(request, response){
+    var operatorType = request.params.operation;
+    var num1 = parseInt(request.query.num1);
+    var num2 = parseInt(request.query.num2);
+    var solution = '';
+    switch(operatorType) {
+        case 'add':
+            solution = num1 + num2;
+            break;
+        case 'sub':
+            solution = num1 > num2 ? num1 - num2 : num2 - num1;
+            break;
+        case 'mult':
+            solution = num1 * num2;
+            break;
+        case 'div':
+            solution = num1 / num2;
+            break;
+        default:
+        //tried to put 10.4.1 but wouldn't allow the second dot operator?
+            response.status(10.4);
+    }
+
+        response.send({
+                operator: operatorType,
+                firstOperand: num1,
+                secondOperand: num2,
+                solution: solution
+            });
+    
 });
-
-
 
 
 
